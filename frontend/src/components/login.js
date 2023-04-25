1f7b9c0246bc5183ff8538e4656c4371095bdb29
Christian Gémesi <christian.gemesi@students.fhnw.ch>
2023-04-25 09:47:05 +0200

    cleanup

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,64 +1,66 @@
 import React, { useState } from 'react';
-import { useNavigate  } from 'react-router-dom';
-import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const navigate = useNavigate ();
+    const navigate = useNavigate();
 
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-
+    const handleLogin = async () => {
         try {
+            // Call the login API
             const response = await fetch('http://localhost:3000/account/', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username, password }),
             });
 
-            if (response.ok) {
-                // redirect to dashboard
-                navigate('/dashboard');
-            } else {
-                console.log(response);
+            if (!response.ok) {
                 throw new Error('Invalid credentials');
             }
 
             const responseData = await response.json();
-            console.log(responseData);
 
-
+            // Call the groups API with the token
             const groupsResponse = await fetch('http://localhost:3000/groups/', {
                 headers: { Authorization: `Token ${responseData.token}` },
             });
 
             if (!groupsResponse.ok) {
-                console.log(groupsResponse);
                 throw new Error('Failed to fetch groups');
             }
 
             const groupsData = await groupsResponse.json();
-            console.log(groupsData);
 
-            // update data object with API response data
+            // Build the data object with the API response data
             const newData = {
                 username: responseData.username,
                 groups: groupsData.map((group) => ({
                     groupId: group.id,
-                    group_name: group.group_name,
-
+                    groupName: group.group_name,
                 })),
             };
 
+            return newData;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const newData = await handleLogin();
             console.log(newData);
 
+            // Redirect to dashboard
+            navigate('/dashboard');
         } catch (error) {
             console.error(error);
         }
     };
-
 
     return (
         <div>
