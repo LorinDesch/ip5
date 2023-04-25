1714528e0338dc02739c1d82b1d94fcdb6ba20f3
Lorin Desch <lorin.desch@students.fhnw.ch>
2023-04-25 09:35:08 +0200

    added login field and button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,37 +4,51 @@ function App() {
     const [selectedOption1, setSelectedOption1] = useState('Ich');
     const [selectedOption2, setSelectedOption2] = useState('Challenge');
     const [selectedOption3, setSelectedOption3] = useState('Gruppen');
-    const [classCheckboxes, setClassCheckboxes] = useState(Array.from(Array(10), () => false));
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
     const options1 = ['Ich', 'Meine Klasse'];
     const options2 = ['Challenge', 'Reduktion Wasserverbrauch', 'Reduktion Stromverbrauch', 'Reduktion Fleischkonsum'];
 
-    const handleCheckboxChange = (index) => {
-        const newClassCheckboxes = [...classCheckboxes];
-        newClassCheckboxes[index] = !newClassCheckboxes[index];
-        setClassCheckboxes(newClassCheckboxes);
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleLogin = () => {
+        console.log(`Username: ${username}, Password: ${password}`);
     };
 
     const handleChange1 = (event) => {
         setSelectedOption1(event.target.value);
-    }
+    };
 
     const handleChange2 = (event) => {
         setSelectedOption2(event.target.value);
-    }
+    };
 
     const handleChange3 = (event) => {
         setSelectedOption3(event.target.value);
-    }
+    };
 
     return (
         <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-            <select value={selectedOption1} onChange={handleChange1}>
-                {options1.map(option => (
-                    <option key={option} value={option}>{option}</option>
-                ))}
-            </select>
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                <select value={selectedOption1} onChange={handleChange1}>
+                    {options1.map(option => (
+                        <option key={option} value={option}>{option}</option>
+                    ))}
+                </select>
+                <form onSubmit={handleLogin} style={{ marginLeft: '1rem' }}>
+                    <input type="text" placeholder="Benutzername" value={username} onChange={handleUsernameChange} />
+                    <input type="password" placeholder="Passwort" value={password} onChange={handlePasswordChange} />
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}>
                 <select value={selectedOption2} onChange={handleChange2}>
                     {options2.map(option => (
                         <option key={option} value={option}>{option}</option>
@@ -46,23 +60,10 @@ function App() {
                     <option value="Gruppen">Gruppen</option>
                     <optgroup label="Klassen">
                         {Array.from(Array(10), (x, index) => index + 1).map(option => (
-                            <option key={`class-${option}`} value={`Klasse ${option}`}>
-                                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                                    <span>{`Klasse ${option}`}</span>
-                                    <input type="checkbox" id={`class-${option}`} checked={classCheckboxes[option - 1]} onChange={() => handleCheckboxChange(option - 1)} />
-                                </div>
-                            </option>
+                            <option key={`class-${option}`} value={`Klasse ${option}`}>{`Klasse ${option}`}</option>
                         ))}
                     </optgroup>
                 </select>
-                <div style={{ position: 'absolute', top: '100%', left: 0, width: '100%' }}>
-                    {Array.from(Array(10), (x, index) => index + 1).map(option => (
-                        <div key={`checkbox-${option}`}>
-                            <input type="checkbox" id={`class-${option}`} checked={classCheckboxes[option - 1]} onChange={() => handleCheckboxChange(option - 1)} />
-                            <label htmlFor={`class-${option}`}>{`Klasse ${option}`}</label>
-                        </div>
-                    ))}
-                </div>
             </div>
         </div>
     );
