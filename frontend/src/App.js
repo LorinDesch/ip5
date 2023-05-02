18283e20f643a846cbbff556a244a7f759aef3de
Christian Gémesi <christian.gemesi@students.fhnw.ch>
2023-05-02 21:01:06 +0200

    Fix merge conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
+
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Dashboard from "./components/dashboard";
-
-
-function App() {
+const App = () => {
     return (
         <Router>
             <Routes>
