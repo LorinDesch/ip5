b5fc32b0a95dd1ebb925fe0b3cd4f97ee7bc9705
Christian Gémesi <christian.gemesi@students.fhnw.ch>
2023-05-03 15:24:04 +0200

    fix

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 import Dropdown1 from './Dropdown1';
 import Dropdown2 from './Dropdown2';
 import Dropdown3 from './Dropdown3';
+import ToggleSwitch from "./ToggleSwitch";
+import Login from "./Login";
 
 function Dashboard() {
     const [selectedOption1, setSelectedOption1] = useState('Ich');
     const [selectedOption2, setSelectedOption2] = useState('Challenge');
     const [selectedOption3, setSelectedOption3] = useState([]);
     const [selectedClasses, setSelectedClasses] = useState([]);
+    const [isOn, setIsOn] = useState(false);
 
     const options3 = Array.from(Array(10), (_, index) => ({
         value: `Klasse ${index + 1}`,
@@ -34,19 +37,53 @@ function Dashboard() {
         }
     };
 
+    const handleToggle = (checked) => {
+        setIsOn(checked);
+    };
+
     return (
-        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', borderBottom: '1px solid black', paddingBottom: '1rem' }}>
-            <div style={{ display: 'flex', alignItems: 'center', marginRight: '1rem' }}>
-                <Dropdown1 selectedOption={selectedOption1} handleChange={handleChange1} />
+        <div>
+            <div style={{
+                display: 'grid',
+                gridTemplateColumns: '1.2fr 2fr 1fr auto',
+                alignItems: 'center',
+                borderBottom: '1px solid black',
+                paddingBottom: '1rem',
+                filter: isOn ? 'blur(5px)' : 'none' /* Filter hinzufügen */
+            }}>
+                <div style={{gridColumn: '1'}}>
+                    <Dropdown1 selectedOption={selectedOption1} handleChange={handleChange1}/>
+                </div>
+                <div style={{gridColumn: '2', justifySelf: 'center'}}>
+                    <Dropdown2 selectedOption={selectedOption2} handleChange={handleChange2}/>
+                </div>
+                <div style={{gridColumn: '3', justifySelf: 'end', marginRight: '2rem'}}>
+                    <ToggleSwitch leftText="Fake Data" rightText="Real Data" isOn={isOn} handleToggle={handleToggle} style={{marginRight: '1rem'}}/>
+                </div>
+                <div style={{gridColumn: '4', justifySelf: 'end', marginRight: '0.5rem'}}>
+                    <Dropdown3 options={options3} selectedOptions={selectedOption3} onChange={handleChange3}/>
+                </div>
             </div>
-            <div style={{ display: 'flex', alignItems: 'center', marginRight: '1rem' }}>
-                <Dropdown2 selectedOption={selectedOption2} handleChange={handleChange2} />
-            </div>
-            <div style={{ display: 'flex', alignItems: 'center', position: 'relative' }}>
-                <Dropdown3 options={options3} selectedOptions={selectedOption3} onChange={handleChange3} />
-            </div>
+            {isOn && (
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '100vh',
+                    position: 'fixed',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    //background: 'rgba(255, 255, 255, 0.01)', /* Hintergrund hinzufügen */
+                    zIndex: 999 /* Z-Index hinzufügen */
+                }}>
+                    <Login />
+                </div>
+            )}
         </div>
     );
+
 }
 
 export default Dashboard;
