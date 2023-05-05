71672567c2059555de82db7f47d5ee8c2076e9e5
Christian Gémesi <christian.gemesi@students.fhnw.ch>
2023-05-05 10:46:51 +0200

    worked on 3rd chart

--- frontend/src/components/Charts/Difficulty.js
+++ frontend/src/components/Charts/Difficulty.js
@@ -3,8 +3,10 @@ import * as d3 from 'd3';
 
 function DifficultyBar({ data, width, height }) {
     const leftLabel = ["leicht", "mittel", "schwer"];
-    const rightLabel = ["stark eingeschränkt", "mittelmässig eingeschränkt", "stark eingeschränkt"];
+    const rightLabel = ["leicht", "mittel", "stark"];
     const klassen = ["Klasse 1", "Klasse 2", "Klasse 3", "Klasse 4", "Klasse 5", "Klasse 6"];
+    const yAxisLabelLeft = "Schwierigkeit";
+    const yAxisLabelRight = "Einschränkung";
     const dataRight = [0.5, 0.1, 0.8, 0.7, 1, 0.3];
     const dataLeft = [0.7, 0.2, 0.1, 0.3, 0.5, 0.3];
 
@@ -16,30 +18,40 @@ function DifficultyBar({ data, width, height }) {
             .attr('height', height)
             .style('overflow', 'visible');
 
-        // Define the scales for the x-axis and y-axis
+        const margin = { top: 10, right: 10, bottom: 20, left: 40 };
+
         const xScale = d3.scaleBand()
             .domain(klassen)
-            .range([0, width])
+            .range([margin.left, width - margin.right])
             .padding(0.5);
 
         const yScale = d3.scaleLinear()
             .domain([0, d3.max(dataLeft.concat(dataRight))])
-            .range([height, 0]);
+            .range([height - margin.bottom, margin.top]);
 
-        // Draw the left bars
+        const xAxis = d3.axisBottom(xScale)
+            .tickValues(klassen);
+
+        const leftYAxis = d3.axisLeft(yScale)
+            .ticks(3)
+            .tickFormat((d, i) => leftLabel[i]);
+
+        const rightYAxis = d3.axisRight(yScale)
+            .ticks(3)
+            .tickFormat((d, i) => rightLabel[i]);
+
         const leftBars = svg.append('g')
             .attr('class', 'left-bars')
             .selectAll('rect')
             .data(dataLeft)
             .enter()
             .append('rect')
-            .attr('x', (d, i) => xScale(klassen[i]) - xScale.bandwidth()/2 +15)
+            .attr('x', (d, i) => xScale(klassen[i]) - xScale.bandwidth()/2 + 13)
             .attr('y', d => yScale(d))
             .attr('width', xScale.bandwidth()/2)
-            .attr('height', d => height - yScale(d))
+            .attr('height', d => height - yScale(d) - margin.bottom)
             .attr('fill', 'steelblue');
 
-        // Draw the right bars
         const rightBars = svg.append('g')
             .attr('class', 'right-bars')
             .selectAll('rect')
@@ -49,23 +61,48 @@ function DifficultyBar({ data, width, height }) {
             .attr('x', (d, i) => xScale(klassen[i]) + xScale.bandwidth()/2)
             .attr('y', d => yScale(d))
             .attr('width', xScale.bandwidth()/2)
-            .attr('height', d => height - yScale(d))
+            .attr('height', d => height - yScale(d) - margin.bottom)
             .attr('fill', 'orange');
-
-        // Add x-axis and y-axis
-        const xAxis = d3.axisBottom(xScale)
-            .tickValues(klassen);
-
-        const yAxis = d3.axisLeft(yScale);
 
         svg.append('g')
             .attr('class', 'x-axis')
-            .attr('transform', `translate(0, ${height})`)
+            .attr('transform', `translate(0, ${height - margin.bottom})`)
             .call(xAxis);
+
 
         svg.append('g')
             .attr('class', 'y-axis')
-            .call(yAxis);
+            .attr('transform', `translate(${margin.left}, 0)`)
+            .call(leftYAxis)
+            .style('color', 'steelblue');
+
+        svg.append('g')
+            .attr('class', 'y-axis')
+            .attr('transform', `translate(${width - margin.right}, 0)`)
+            .call(rightYAxis)
+            .style('color', 'orange');
+
+        // Add left y-axis label
+        svg.append('text')
+            .attr('class', 'axis-label')
+            .attr('x', margin.left - 50)
+            .attr('y', height / 2)
+            .attr('text-anchor', 'middle')
+            .attr('alignment-baseline', 'central')
+            .attr('transform', 'rotate(-90, ' + (margin.left - 50) + ', ' + (height / 2) + ')')
+            .text(yAxisLabelLeft)
+            .style('fill', 'steelblue');
+
+        // Add right y-axis label
+        svg.append('text')
+            .attr('class', 'axis-label')
+            .attr('x', width - margin.right + 50)
+            .attr('y', height / 2)
+            .attr('text-anchor', 'middle')
+            .attr('alignment-baseline', 'central')
+            .attr('transform', 'rotate(-90, ' + (width - margin.right + 50) + ', ' + (height / 2) + ')')
+            .text(yAxisLabelRight)
+            .style('fill', 'orange');
 
     }, [data, height, width]);
 
--- frontend/src/components/DifficultyComparison.js
+++ frontend/src/components/DifficultyComparison.js
@@ -1,18 +1,26 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import TopNavbar from './TopNavbar';
 import Difficulty from "./Charts/Difficulty";
 
-const DifficultyComparison = ({ data, width, height }) => {
+const DifficultyComparison = ({data, width, height}) => {
     const [isOn, setIsOn] = useState(false);
 
     data = [200, 30, 160, 50, 300, 400];
 
     return (
         <div>
-            <TopNavbar isOn={isOn} setIsOn={setIsOn} />
+            <TopNavbar isOn={isOn} setIsOn={setIsOn}/>
 
             <h1>Difficulty Comparison</h1>
-            <Difficulty data={data} width={400} height={400} />
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "100vh"
+            }}>
+                <Difficulty data={data} width={600} height={400}/>
+            </div>
         </div>
     );
 };
