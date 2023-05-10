ecc4321d4b874f175651f86159252b71bf83cbf7
Lorin Desch <lorin.desch@students.fhnw.ch>
2023-05-10 10:09:06 +0200

    resizing dropdowns

--- frontend/src/components/Dropdown1.js
+++ frontend/src/components/Dropdown1.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Dropdown1 = ({ fakeData, onChange, styling, options, changeableFakeData, setChangeableFakeData,selectedOptions1 }) => {
+const Dropdown1 = ({ fakeData, onChange, styling, options, changeableFakeData, setChangeableFakeData, selectedOptions1 }) => {
 
     return (
-        <select value={selectedOptions1} onChange={onChange} style={styling}>
+        <select value={selectedOptions1} onChange={onChange} style={{...styling, width: '16rem'}}>
             {options.map((option) => (
                 <option key={option} style={styling}>
                     {option}
