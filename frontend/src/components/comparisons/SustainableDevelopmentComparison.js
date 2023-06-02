86a1de0156d17e9566b7fac7e200ce6903bdb737
Christian Gémesi <christian.gemesi@students.fhnw.ch>
2023-06-02 19:09:02 +0200

    cleanup

--- frontend/src/components/comparisons/SustainableDevelopmentComparison.js
+++ frontend/src/components/comparisons/SustainableDevelopmentComparison.js
@@ -45,6 +45,7 @@ const SustainabilityComparison = ({
                                       sustainableDevelopmentlabelLeft,
                                       sustainableDevelopmentValueRightBar,
                                       sustainableDevelopmentValueLeftBar,
+
                                   }) => {
 
     return (
