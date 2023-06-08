3478125e17876bd0a1c2efc0872a8ae16599b8c1
Lorin Desch <lorin.desch@students.fhnw.ch>
2023-06-08 15:23:09 +0200

    sidenavbar

--- frontend/src/components/SideNavbar.js
+++ frontend/src/components/SideNavbar.js
@@ -1,123 +1,3 @@
-// import React, {useState, useEffect} from 'react';
-// import {FaHome, FaChartBar} from 'react-icons/fa';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-//
-//
-// /**
-//  * Component for the side navigation bar.
-//  *
-//  * @returns {JSX.Element} The rendered SideNavbar component.
-//  */
-// const SideNavbar = () => {
-//     const [containerHeight, setContainerHeight] = useState(0);
-//
-//     useEffect(() => {
-//         const updateContainerHeight = () => {
-//             setContainerHeight(window.innerHeight);
-//         };
-//         window.addEventListener('resize', updateContainerHeight);
-//         updateContainerHeight();
-//         return () => window.removeEventListener('resize', updateContainerHeight);
-//     }, []);
-//
-//     const handleIconHover = (e) => {
-//         e.target.style.color = '#5E7839';
-//         e.target.style.transform = 'scale(1.2)';
-//     };
-//
-//     const handleIconLeave = (e) => {
-//         e.target.style.color = 'black';
-//         e.target.style.transform = 'scale(1)';
-//     };
-//
-//     return (
-//         <nav>
-//             <div>
-//                 <div>Home</div>
-//                 <FaHome
-//                     size={'3vw'}
-//                     className="nav-icon rounded border border-secondary p-2"
-//                     onMouseEnter={handleIconHover}
-//                     onMouseLeave={handleIconLeave}
-//                     onClick={() => (window.location.href = '/')}
-//                 />
-//             </div>
-//             <div >
-//                 <div >Schwierigkeit</div>
-//                 <FaChartBar
-//                     size={'3vw'}
-//                     className="nav-icon rounded border border-secondary p-2"
-//                     onMouseEnter={handleIconHover}
-//                     onMouseLeave={handleIconLeave}
-//                     onClick={() => (window.location.href = '/schwierigkeit')}
-//                 />
-//             </div>
-//             <div >
-//                 <div >Ein-<br/>schränkung</div>
-//                 <FaChartBar
-//                     size={'3vw'}
-//                     className="nav-icon rounded border border-secondary p-2"
-//                     onMouseEnter={handleIconHover}
-//                     onMouseLeave={handleIconLeave}
-//                     onClick={() => (window.location.href = '/einschraenkung')}
-//                 />
-//             </div>
-//             <div >
-//                 <div >Umfeld</div>
-//                 <FaChartBar
-//                     size={'3vw'}
-//                     className="nav-icon rounded border border-secondary p-2"
-//                     onMouseEnter={handleIconHover}
-//                     onMouseLeave={handleIconLeave}
-//                     onClick={() => (window.location.href = '/umfeld')}
-//                 />
-//             </div>
-//             <div >
-//                 <div >aktueller<br />Beitrag</div>
-//                 <FaChartBar
-//                     size={'3vw'}
-//                     className="nav-icon rounded border border-secondary p-2"
-//                     onMouseEnter={handleIconHover}
-//                     onMouseLeave={handleIconLeave}
-//                     onClick={() => (window.location.href = '/aktuellerbeitrag')}
-//                 />
-//             </div>
-//             <div  >
-//                 <div>Ermöglicht mir</div>
-//                 <FaChartBar
-//                     size={'3vw'}
-//                     className="nav-icon rounded border border-secondary p-2"
-//                     onMouseEnter={handleIconHover}
-//                     onMouseLeave={handleIconLeave}
-//                     onClick={() => (window.location.href = '/erlaubtmir')}
-//                 />
-//             </div>
-//
-//             <div>
-//                 <div>nachhaltigen<br />Entwicklung</div>
-//                 <FaChartBar
-//                     size={'3vw'}
-//                     className="nav-icon rounded border border-secondary p-2"
-//                     onMouseEnter={handleIconHover}
-//                     onMouseLeave={handleIconLeave}
-//                     onClick={() => (window.location.href = '/nachhaltigeentwicklung')}
-//                 />
-//             </div>
-//         </nav>
-//     );
-// };
-//
-// export default SideNavbar;
-//
-//
-//
-//
-
-
-
-
-
-
 import React, { useState, useEffect } from 'react';
 import { FaHome, FaChartBar } from 'react-icons/fa';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -162,8 +42,8 @@ const SideNavbar = () => {
             </div>
             {isSidebarOpen && (
                 <>
-                    <div>
-                        <div>Home</div>
+                    <div className="sidebar-item">
+                        <div className="sidebar-text">Home</div>
                         <FaHome
                             size={'3vw'}
                             className="nav-icon rounded border border-secondary p-2"
@@ -172,8 +52,8 @@ const SideNavbar = () => {
                             onClick={() => (window.location.href = '/')}
                         />
                     </div>
-                    <div>
-                        <div>Schwierigkeit</div>
+                    <div className="sidebar-item">
+                        <div className="sidebar-text">Schwierigkeit</div>
                         <FaChartBar
                             size={'3vw'}
                             className="nav-icon rounded border border-secondary p-2"
@@ -182,8 +62,8 @@ const SideNavbar = () => {
                             onClick={() => (window.location.href = '/schwierigkeit')}
                         />
                     </div>
-                    <div>
-                        <div>Einschränkung</div>
+                    <div className="sidebar-item">
+                        <div className="sidebar-text">Ein-<br/>schränkung</div>
                         <FaChartBar
                             size={'3vw'}
                             className="nav-icon rounded border border-secondary p-2"
@@ -192,8 +72,8 @@ const SideNavbar = () => {
                             onClick={() => (window.location.href = '/einschraenkung')}
                         />
                     </div>
-                    <div>
-                        <div>Umfeld</div>
+                    <div className="sidebar-item">
+                        <div className="sidebar-text">Umfeld</div>
                         <FaChartBar
                             size={'3vw'}
                             className="nav-icon rounded border border-secondary p-2"
@@ -202,8 +82,8 @@ const SideNavbar = () => {
                             onClick={() => (window.location.href = '/umfeld')}
                         />
                     </div>
-                    <div>
-                        <div>aktueller<br />Beitrag</div>
+                    <div className="sidebar-item">
+                        <div className="sidebar-text">aktueller Beitrag</div>
                         <FaChartBar
                             size={'3vw'}
                             className="nav-icon rounded border border-secondary p-2"
@@ -212,8 +92,8 @@ const SideNavbar = () => {
                             onClick={() => (window.location.href = '/aktuellerbeitrag')}
                         />
                     </div>
-                    <div>
-                        <div>Ermöglicht mir</div>
+                    <div className="sidebar-item">
+                        <div className="sidebar-text">Ermöglicht mir</div>
                         <FaChartBar
                             size={'3vw'}
                             className="nav-icon rounded border border-secondary p-2"
@@ -222,8 +102,8 @@ const SideNavbar = () => {
                             onClick={() => (window.location.href = '/erlaubtmir')}
                         />
                     </div>
-                    <div>
-                        <div>nachhaltigen<br />Entwicklung</div>
+                    <div className="sidebar-item">
+                        <div className="sidebar-text">nachhaltigen Entwicklung</div>
                         <FaChartBar
                             size={'3vw'}
                             className="nav-icon rounded border border-secondary p-2"
