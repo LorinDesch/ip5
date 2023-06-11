785e1261acfd0f39e88a72a9af7076216b085a67
Lorin Desch <lorin.desch@students.fhnw.ch>
2023-06-11 16:15:59 +0200

    topbar

--- frontend/src/components/TopNavbar.js
+++ frontend/src/components/TopNavbar.js
@@ -1,7 +1,5 @@
-// TopNavbar.js
-
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
 import Dropdown1 from './Dropdown1';
 import Dropdown2 from './Dropdown2';
 import Dropdown3 from './Dropdown3';
@@ -16,6 +14,7 @@ const TopNavbar = ({
                        setSelectedOption2,
                        setSelectedOption3,
                    }) => {
+
     const dropDownStyling = {
         padding: '12px 38px 12px 12px',
         border: '2px solid black',
@@ -30,7 +29,27 @@ const TopNavbar = ({
         backgroundPosition: 'right 10px center',
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'auto 30%',
+        width: '16rem',
     };
+    const smallDropDownStyling = {
+        padding: '12px 1px 12px 12px',
+        border: '2px solid black',
+        borderRadius: '4px',
+        backgroundColor: '#fff',
+        appearance: 'none',
+        fontFamily: 'sans-serif',
+        fontSize: '15px',
+        cursor: 'pointer',
+        backgroundImage:
+            'url("data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' width=\'10\' height=\'6\' viewBox=\'0 0 10 6\'%3E%3Cpath fill=\'%23000000\' fill-rule=\'evenodd\' d=\'M5 6L0 .666666667 1.11111111 0l3.88888889 3.666666667L8.88888889 0 10 .666666667\'/%3E%3C/svg%3E")',
+        backgroundPosition: 'right 10px center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'auto 30%',
+        width: '300px',
+        textAlign: 'center',
+        alignContent: 'center',
+    };
+
 
     const navigate = useNavigate();
 
@@ -62,44 +81,62 @@ const TopNavbar = ({
     };
 
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
 
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <div className="top-navbar">
-            {window.innerWidth < 768 && (
+            {windowWidth < 768 && (
                 <button className="menu-button" onClick={toggleMenu}>
-                    Menu
+                    <div className={`hamburger ${isMenuOpen ? 'open' : ''}`}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
                 </button>
             )}
-            {(window.innerWidth >= 768 || isMenuOpen) && (
+            {(windowWidth >= 768 || isMenuOpen) && (
                 <>
                     <Dropdown1
                         options={options1}
                         onChange={handleChange1}
-                        styling={dropDownStyling}
+                        styling={windowWidth < 768 ? smallDropDownStyling : dropDownStyling}
                         selectedOptions={selectedOption1}
                         fakeData={fakeData}
                     />
                     <Dropdown2
                         options={options2}
                         onChange={handleChange2}
-                        styling={dropDownStyling}
+                        styling={windowWidth < 768 ? smallDropDownStyling : dropDownStyling}
                         selectedOptions2={selectedOption2}
                         fakeData={fakeData}
                     />
-                    <button onClick={handleLogout} className="btn btn-outline-dark btn-sm">
+                    <button onClick={handleLogout} className="logout-button">
                         logout
                     </button>
                     <Dropdown3
                         options={options3}
                         onChange={handleChange3}
-                        styling={dropDownStyling}
+                        styling={windowWidth < 768 ? smallDropDownStyling : dropDownStyling}
                         selectedOptions={selectedOption3}
                         fakeData={fakeData}
                     />
+
                 </>
             )}
         </div>
